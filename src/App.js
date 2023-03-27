386faf110c3c3e76a76c541c412aacc56c52b74c
Simon Miner <simon.miner@gmail.com>
2023-03-26 21:02:07 -0400

    Continue initial mock-up setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./sonoclock-logo.png";
 import { FormControlLabel, Switch } from '@mui/material';
 
-function App() {
-  var currentTime = formatAMPM(new Date());
-  return (
-    <><div>
-      <img src={logo} alt="Sonoclock - Listen to the time" />
-    </div>
-    <div>
-      <span>
-        <FormControlLabel control={<Switch defaultChecked />} label="Hour" />
-      </span>
-       <span>
-         <FormControlLabel control={<Switch defaultChecked />} label="Minute" />
-       </span>
-     </div>
-     <div>{currentTime}</div>
-     </>
-  );
-}
+export default class App extends Component {
 
-function formatAMPM(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
+    /**
+   * Interval used to increment the clock;
+   */
+   interval;
+
+  constructor(props) {
+    super(props);
+    this.time = props.time !== undefined ? props.time : this.formatAMPM(new Date());
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.time = this.formatAMPM(new Date());
+      this.forceUpdate();
+    }, 1000 );
+  }
+
+  render() {
+    return (
+      <><div>
+        <img src={logo} alt="Sonoclock - Listen to the time" />
+      </div>
+        <div>
+          <span>
+            <FormControlLabel control={<Switch defaultChecked />} label="Hour" />
+          </span>
+          <span>
+            <FormControlLabel control={<Switch defaultChecked />} label="Minute" />
+          </span>
+        </div>
+        <div>{this.time}</div>
+      </>
+    );
+  }
+
+  formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
 }
-export default App;
