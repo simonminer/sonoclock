2a9b23210c639796644316ba2b76a6beb88216a2
Simon Miner <simon.miner@gmail.com>
2023-03-26 22:25:03 -0400

    Format clock time

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ export default class App extends Component {
 
   constructor(props) {
     super(props);
-    this.time = props.time !== undefined ? props.time : this.formatAMPM(new Date());
+    this.time = props.time !== undefined ? props.time : new Date();
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
-      this.time = this.formatAMPM(new Date());
+      this.time = new Date();
       this.forceUpdate();
     }, 1000 );
   }
@@ -34,19 +34,28 @@ export default class App extends Component {
             <FormControlLabel control={<Switch defaultChecked />} label="Minute" />
           </span>
         </div>
-        <div>{this.time}</div>
+        <div className="clock">{this.formatTwelveHourTime(this.time)}</div>
       </>
     );
   }
 
-  formatAMPM(date) {
+  /**
+   * Formats the specified (or current) time as a 12-hour clock.
+   * (i.e. 12:34:56 pm)
+   * @see https://stackoverflow.com/a/8888498/2171535 
+   * @param {Date} date - the date to be formatted
+   * @returns String
+   */
+  formatTwelveHourTime(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    var strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
     return strTime;
   }
 }
